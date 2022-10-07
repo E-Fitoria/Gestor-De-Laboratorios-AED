0280b8ede14b31e9315dcce4b0e4373eb94d75f1
unknown <fitoriajf@gmail.com>
2022-10-07 14:43:16 -0600

    Subiendo primeros archivos

---
+++ src/TransferirDatos/ModeloDeTransferencia.java
@@ -0,0 +1,29 @@
+
+package TransferirDatos;
+
+/**
+ *
+ * @author Usuario
+ * 
+ * esta clase la utilizamos como un modelo para poder pasar string al formulario 
+ * PanelVerAsignacion del paquete interfaz
+ */
+public class ModeloDeTransferencia {
+    
+    private String asignacion;
+
+    /**
+     * @return the asignacion
+     */
+    public String getAsignacion() {
+        return asignacion;
+    }
+
+    /**
+     * @param asignacion the asignacion to set
+     */
+    public void setAsignacion(String asignacion) {
+        this.asignacion = asignacion;
+    }
+    
+}
